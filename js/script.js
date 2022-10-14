811c09cb07d49e4e6bfc6647a55c653b0bee97f4
Marek Brzezinski <brzezi14@gmail.com>
2022-10-14 20:36:28 +0200

    JS changed name element

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 console.log ("bla bla bla")
 
-let formAmountEuro = document.querySelector(".js-formAmountEuro");
-let formExchangeRate = document.querySelector(".js-formExchangeRate");
-let form = document.querySelector(".js-form");
-let formResult = document.querySelector(".js-formResult");
+let formAmountEuroElement = document.querySelector(".js-formAmountEuro");
+let formExchangeRateElement = document.querySelector(".js-formExchangeRate");
+let formElement = document.querySelector(".js-form");
+let formResultElement = document.querySelector(".js-formResult");
 
-form.addEventListener ("submit", (event) => {
+formElement.addEventListener ("submit", (event) => {
     event.preventDefault();
 
-    let euro = formAmountEuro.value;
-    let pln = formExchangeRate.value;
+    let euro = formAmountEuroElement.value;
+    let pln = formExchangeRateElement.value;
     let result = euro * pln;
-    formResult.innerText = "Result: " + result.toFixed(2) + " PLN";
+    formResultElement.innerText = "Result: " + result.toFixed(2) + " PLN";
 })
 
 
